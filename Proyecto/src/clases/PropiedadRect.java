4b690c6c03e94da5c54223a5da01cad1fe0222f6
Josué Méndez <42630284+Yossu502@users.noreply.github.com>
2021-03-07 11:50:26 -0600

    Clase propiedad de rectangulos

---
+++ Proyecto/src/clases/PropiedadRect.java
@@ -0,0 +1,50 @@
+package clases;
+
+public class PropiedadRect {
+    private int iniciax;
+    private int iniciay;
+    private int ancho;
+    private int largo;
+
+    public PropiedadRect(int iniciax, int iniciay, int ancho, int largo) {
+        this.iniciax = iniciax;
+        this.iniciay = iniciay;
+        this.ancho = ancho;
+        this.largo = largo;
+    }
+
+    public int getIniciax() {
+        return iniciax;
+    }
+
+    public void setIniciax(int iniciax) {
+        this.iniciax = iniciax;
+    }
+
+    public int getIniciay() {
+        return iniciay;
+    }
+
+    public void setIniciay(int iniciay) {
+        this.iniciay = iniciay;
+    }
+
+    public int getAncho() {
+        return ancho;
+    }
+
+    public void setAncho(int ancho) {
+        this.ancho = ancho;
+    }
+
+    public int getLargo() {
+        return largo;
+    }
+
+    public void setLargo(int largo) {
+        this.largo = largo;
+    }
+
+   
+    
+}
